2135e173ec5e6143808a134e459343637cba9758
mre55 <misganawmihreteab>
2021-11-17 21:33:21 +0300

    Add mobile menu for both pages

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,20 @@
-const check = document.querySelector('.h1');
+const openMobileMenu = document.querySelector('.hamburger-icon');
+const closeMobileMenu = document.querySelector('.close-button');
+const menuContainer = document.querySelector('.mobile-menu-list');
+const humburgerButton = document.querySelector('.hamburger-icon');
+const mobile_menu = document.querySelector('.mobile-menu')
 
-check.addEventListener('click', () => {
+closeMobileMenu.addEventListener('click', ()=> {
+    mobile_menu.classList.remove('active');
+    humburgerButton.classList.remove('hide');
+})
 
-});
+menuContainer.addEventListener('click', ()=> {
+    mobile_menu.classList.remove('active');
+    humburgerButton.classList.remove('hide');
+})
+
+openMobileMenu.addEventListener('click', ()=> {
+    mobile_menu.classList.add('active');
+    humburgerButton.classList.add('hide');
+})
