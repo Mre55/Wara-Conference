f08f46a78833ad630df42a8c444e16a5a721169c
mre55 <misganawmihreteab>
2021-11-18 01:38:17 +0300

    Add content for dynamic element of feature speaker section

--- JS/script.js
+++ JS/script.js
@@ -10,39 +10,39 @@ const showMoreBtn = document.querySelector('.show-more-btn');
 const speakers = [
   {
     name: 'Bishop Samuel Smith',
-    position: 'General Chairman of the Apostolic World Christian Fellowship [AWCF].',
+    title: 'General Chairman of the Apostolic World Christian Fellowship [AWCF].',
     description: 'Bishop Samuel Smith is honored among Apostolics across the globe as a modern day Apostle.',
     speaker_image: './Images/smith.jpeg',
   },
   {
     name: 'Bishop Degu Kebede',
-    position: 'General Chairman of the Apostolic Church of Ethiopia [ACE].',
+    title: 'General Chairman of the Apostolic Church of Ethiopia [ACE].',
     description: 'Bishop Degu Kebede is honored among Apostolics across the globe as a modern day Apostle.',
     speaker_image: './Images/degu.jpeg',
   },
   {
-    name: 'Bishop Samuel Smith',
-    position: 'General Chairman of the Apostolic World Christian Fellowship [AWCF].',
-    description: 'Bishop Samuel Smith is honored among Apostolics across the globe as a modern day Apostle.',
-    speaker_image: './Images/smith.jpeg',
+    name: 'Bishop Teklemariam Gezahagne',
+    title: 'Former general Chairman of the Apostolic Church of Ethiopia [ACE].',
+    description: 'Bishop Teklemariam Gezahegn(Dr.) began to embrace and preach the doctrines of Oneness and Jesus Name baptism during this time. In recent years, the ACI has become more ethnically diverse.',
+    speaker_image: './Images/tekele.jpeg',
   },
   {
-    name: 'Bishop Degu Kebede',
-    position: 'General Chairman of the Apostolic Church of Ethiopia [ACE].',
-    description: 'Bishop Degu Kebede is honored among Apostolics across the globe as a modern day Apostle.',
-    speaker_image: './Images/degu.jpeg',
+    name: 'Dr. C.H. Braddy',
+    title: 'CEO and Chairman of Faith Apostolic Ministries',
+    description: 'Dr. C.H. Braddy has stood before Presidents, Heads of State and has traveled the world... Physician, scientist, educator, author, educator or JUST PREACHER...Dr. C.H. Braddy, Senior Pastor and Founder of Now Faith!',
+    speaker_image: './Images/brady.jpeg',
   },
   {
-    name: 'Bishop Samuel Smith',
-    position: 'General Chairman of the Apostolic World Christian Fellowship [AWCF].',
-    description: 'Bishop Samuel Smith is honored among Apostolics across the globe as a modern day Apostle.',
-    speaker_image: './Images/smith.jpeg',
+    name: 'Pastor Luke Smith',
+    title: 'General Chairman of International Ministries Chairman at [AWCF].',
+    description: 'Luke Benjamin Smith became the pastor of Linden Heights Baptist Church in 2010. He is a native of Washington, D.C. Luke studied at New Orleans Baptist Theological Seminary.',
+    speaker_image: './Images/lukesmith.jpeg',
   },
   {
-    name: 'Bishop Degu Kebede',
-    position: 'General Chairman of the Apostolic Church of Ethiopia [ACE].',
-    description: 'Bishop Degu Kebede is honored among Apostolics across the globe as a modern day Apostle.',
-    speaker_image: './Images/degu.jpeg',
+    name: 'Pastor Elias Shebabaw',
+    title: 'Chairman of Apostolic Books Association at the Apostolic Church of Ethiopia [ACE].',
+    description: 'Pastor Elias Shebabaw have a book called "The secret of Trinity tested with the light of One God".',
+    speaker_image: './Images/elias.jpeg',
   },
 ];
 
@@ -71,8 +71,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="speaker-name">
                         <h5>${speaker.name}</h5>
                     </div>
-                    <div class="speaker-position">
-                        <p>${speaker.position}</p>
+                    <div class="speaker-title">
+                        <p>${speaker.title}</p>
                     </div>
                     <div class="speaker-detail">
                         <p>${speaker.description}</p>
